5bfc6ceae21b64694f80757d4d87ae9b0b9dc532
Justin Egan <justin.d.egan@gmail.com>
2022-12-26 14:51:27 -0500

    clean up function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const timer = setTimeout(() => {
+      console.log("Check validity")
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    return () => {
+      console.log("Cleanup function")
+      clearTimeout(timer)
+    }
   }, [enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
